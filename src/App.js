187ea6cd9b5981f6db7b0f424652405de5033529
Cristobal Licea <cris.licea@gmail.com>
2023-03-30 13:01:05 -0500

    added components folder and Homepage, Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import './App.css';
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+  useNavigate
+} from 'react-router-dom';
+import { Homepage, Navbar } from './components';
+
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element ={<Layout />}>
+          <Route index element={<Homepage/> } />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      <Navbar />
+      <Outlet/>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as Homepage } from './Homepage/Homepage.jsx';
+export { default as Navbar } from './Navbar/Navbar.jsx';
